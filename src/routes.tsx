4cdd4124a2fe76dc99fbc60e18f078f43b262332
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-03-10 11:09:36 -0300

    troquei os imports padrões por lazy e adicionei um suspense

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,28 +1,31 @@
-import Inicio from 'pages/Inicio';
-import Cardapio from 'pages/Cardapio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from 'components/Menu';
-import PaginaPadrao from 'components/PaginaPadrao';
-import Sobre from 'pages/Sobre';
 import Footer from 'components/Footer';
-import NotFound from 'pages/NotFound';
-import Prato from 'pages/Prato';
+import { lazy, Suspense } from 'react';
+
+const Inicio = lazy(() => import('pages/Inicio'));
+const Cardapio = lazy(() => import('pages/Cardapio'));
+const PaginaPadrao = lazy(() => import('components/PaginaPadrao'));
+const Sobre = lazy(() => import('pages/Sobre'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const Prato = lazy(() => import('pages/Prato'));
 
 export default function AppRouter() {
   return (
     <main className='container'>
       <Router>
         <Menu />
-
-        <Routes>
-          <Route path='/' element={<PaginaPadrao />}>
-            <Route index element={<Inicio />} />
-            <Route path='cardapio' element={<Cardapio />} />
-            <Route path='sobre' element={<Sobre />} />
-          </Route>
-          <Route path='prato/:id' element={<Prato />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<p>Carregando...</p>}>
+          <Routes>
+            <Route path='/' element={<PaginaPadrao />}>
+              <Route index element={<Inicio />} />
+              <Route path='cardapio' element={<Cardapio />} />
+              <Route path='sobre' element={<Sobre />} />
+            </Route>
+            <Route path='prato/:id' element={<Prato />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </Router>
     </main>
